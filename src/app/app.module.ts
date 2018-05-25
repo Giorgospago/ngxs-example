77ccb59c37340b722a9cf36400c4d0821c897694
mixalistzikas <mixalistzikas@gmail.com>
2018-05-25 13:20:04 +0300

    example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { addCoinReducer } from './reducers/blockchain.reducer';
 
 import { AppComponent } from './app.component';
+import { BlockchainComponent } from './blockchain/blockchain.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      BlockchainComponent,
+      DisplayComponent
+   ],
+   imports: [
+      BrowserModule,
+      NgbModule.forRoot(),
+      StoreModule.forRoot({ blockchain: addCoinReducer }),
+      ReactiveFormsModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
